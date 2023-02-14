5382d364f3e5564825284a53c02c26686c9f626d
webspaceadam <adam.gniady@flipapp.de>
2023-02-14 16:40:36 +0100

    removed app setting env

--- apps/swirl-docs/src/components/Search/Algolia.tsx
+++ apps/swirl-docs/src/components/Search/Algolia.tsx
@@ -27,8 +27,8 @@ function createAlgoliaClient() {
   }
 
   const searchClient = algoliasearch(
-    process.env.APPSETTING_NEXT_PUBLIC_ALGOLIA_APP_ID!!,
-    process.env.APPSETTING_NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY!!
+    process.env.NEXT_PUBLIC_ALGOLIA_APP_ID!!,
+    process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY!!
   );
 
   return searchClient;
